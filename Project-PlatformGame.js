bc9d45a8e2da090b7563dfa919c5ead4c30429e0
Todor Krastev <to6esko69@gmail.com>
2016-02-04 17:51:47 +0200

    change normal size

--- Project-PlatformGame.js
+++ Project-PlatformGame.js
@@ -85,7 +85,7 @@ Lava.prototype.type = 'lava';
 
 function Coin(pos) {
 	this.basePos = this.pos = pos.plus(new Vector(0.2, 0.1));
-	this.size = new Vector(0.6, 0.9);
+	this.size = new Vector(0.6, 0.6);
 	this.wobble = Math.random() * Math.PI * 2;
 }
 Coin.prototype.type = 'coin';
